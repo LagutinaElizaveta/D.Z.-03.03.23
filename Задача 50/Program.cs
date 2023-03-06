2dafd5bba29e55c3e8bea12b144a27a73f325e4d
Lagutina Elizaveta <below8585@mail.ru>
2023-03-06 10:50:04 +0300

    Готовое домашнее задание

---
+++ Задача 50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+// Ввод параметров массива
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int minValue = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int maxValue = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер строки элемента массива: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите номер столбца элемента массива: ");
+int m = int.Parse(Console.ReadLine());
+/// Метод создания массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+// Метод вывода массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Заполнение массива
+int[,] array = GetArray(rows, colums, minValue, maxValue);
+// Вывод массива (при необходимости)
+PrintArray(array);
+// Вывод результатов поиска элемента по индексам
+if (n >= rows || m >= colums) { Console.WriteLine("Элемента с такими индексами не существует"); }
+else { Console.WriteLine($"Элемент {n}-й строки {m}-го столбца равен {array[n, m]}"); }
---
+++ Задача 52/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Ввод параметров массива
+Console.WriteLine("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int colums = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента массива: ");
+int minValue = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента массива: ");
+int maxValue = int.Parse(Console.ReadLine());
+// Метод создания массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+// Метод вывода массива
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+// Заполнение массива
+int[,] array = GetArray(rows, colums, minValue, maxValue);
+// Вывод массива
+PrintArray(array);
+// Метод поиска среднего арифметического по столбцам
+void ArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double avarage = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            avarage = avarage + array[i, j];
+        }
+        avarage = avarage / rows;
+        Console.Write($" {avarage:f2}; ");
+    }
+}
+// Вывод среднего арифметического
+ArithmeticMean(array);
